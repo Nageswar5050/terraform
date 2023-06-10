bda6c1b548b5aee2d61bcf9be8df0f42bc0f3345
Nageswar5050 <nageswar0550@gmail.com>
2023-06-10 20:53:10 +0530

    "igw and ngw added in main.tf"

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
-locals {
-  name = "Prod"
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "(var.name)-VPC_1"
+  }
 }
 
-resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/24"
-    tags = {
-        Name = "(local.name)-VPC_1"
-    }
+resource "aws_internet_gateway" "igw1" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "(var.name)-IGW_1"
+  }
+}
+
+resource "aws_nat_gateway" "ngw1" {
+  subnet_id = aws_subnet.pri_sub1.id
+  tags = {
+    Name = "(var.name)-NGW_1"
+  }
 }
