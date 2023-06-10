5a8c85e5904757c6862e9a26c1c5e1fe174577a7
Nageswar5050 <nageswar0550@gmail.com>
2023-06-10 20:53:56 +0530

    "provider file and varaiable file modified"

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>4.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "${var.region["region_1"]}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+  region     = var.region["region_1"]
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
--- varaiable.tf
+++ varaiable.tf
@@ -1,17 +1,32 @@
 variable "region" {
-    type = map
-    default = {
-        region_1 = "ap-south-1"
-        region_2 = "ap-south-2"
-    }
+  type = map(any)
+  default = {
+    region_1 = "ap-south-1"
+    region_2 = "ap-south-2"
+  }
 }
 
 variable "access_key" {
-    type = string
-    default = "AKIATHTLCWI2J2EYOTKG"
+  type    = string
+  default = "AKIATHTLCWI2J2EYOTKG"
 }
 
 variable "secret_key" {
-    type = string
-    default = "yY+H5CErvPklTyIea61uEVWwdusVyno81gqE3kiA"
+  type    = string
+  default = "yY+H5CErvPklTyIea61uEVWwdusVyno81gqE3kiA"
+}
+
+variable "az_1" {
+  type    = list(any)
+  default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+}
+
+variable "az_2" {
+  type    = list(any)
+  default = ["ap-south-2a", "ap-south-2b", "ap-south-2c"]
+}
+
+variable "name" {
+  type    = string
+  default = "Prod"
 }
